2cb42f43d24646cb1dcc9cb75508bd33f37d832c
Aniruddha Maru <aniruddhamaru@gmail.com>
2019-06-06 11:56:41 -0700

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "subscription_name" {
   value       = "${google_pubsub_subscription.subscription.name}"
   description = "Name of the Subscription"
 }
-
-output "dataflow_job_name" {
-  value       = "${google_dataflow_job.dataflow.name}"
-  description = "Dataflow job name"
-}
